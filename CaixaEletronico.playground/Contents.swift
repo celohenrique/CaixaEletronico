000f9e4ab036cedd0f560c4974d7c109a7a46c76
Marcelo Henrique <celohenriquech7@gmail.com>
2023-02-05 17:25:25 -0300

    finalizado

--- CaixaEletronico.playground/Contents.swift
+++ CaixaEletronico.playground/Contents.swift
@@ -1,74 +1,31 @@
 import UIKit
 
+let cedulasDisponiveis = [50, 10, 5, 2]
 
-func saque(valor: Int, disponiveis: [Int]) -> String {
-    var result = ""
-    
-    if valor == 8 {
-        result = "2 nota(s) de 4"
-    } else {
-        for cedula in disponiveis {
-            if valor >= cedula {
-                let quantidade = valor / cedula
-                result += "\(quantidade) nota(s) de \(cedula), "
-                let restante = valor - quantidade * cedula
-                if restante > 0 {
-                    let novasDisponiveis = disponiveis.filter { $0 <= restante }
-                    result += saque(valor: restante, disponiveis: novasDisponiveis)
-                    break
-                }
-            }
-        }
+func saque(valor: Int) -> String {
+  if valor == 0 {
+    return ""
+  }
+
+  for cedula in cedulasDisponiveis {
+    if cedula <= valor {
+      let resultadoParcial = saque(valor: valor - cedula)
+
+      if resultadoParcial != "Não é possível realizar o saque." {
+        return "\(cedula), \(resultadoParcial)"
+      }
     }
-    
-    if result.count > 0 {
-        result.removeLast(2)
-    }
-    
-    return result
+  }
+
+  return "Não é possível realizar o saque."
 }
-print(saque(valor: 8, disponiveis: [50, 10, 5, 2]))
-print(saque(valor: 75, disponiveis: [50, 10, 5, 2]))
-print(saque(valor: 30, disponiveis: [50, 10, 5, 2]))
-print(saque(valor: 600, disponiveis: [50, 10, 5, 2]))
-print(saque(valor: 8, disponiveis: [50, 10, 5, 2]))
+
+print(saque(valor: 205))
+print(saque(valor: 75))
+print(saque(valor: 30))
+print(saque(valor: 600))
+print(saque(valor: 8))
 
 
-
-
-
-
-//enum Cedula: Int {
-//  case cinquenta = 50
-//  case dez = 10
-//  case cinco = 5
-//  case dois = 2
-//}
-//
-//func saque(valor: Int, cedulasDisponiveis: [Cedula]) -> String {
-//
-//  var resultado = ""
-//  var valorRestante = valor
-//
-//  for cedula in cedulasDisponiveis {
-//
-//    while valorRestante >= cedula.rawValue {
-//
-//      let quantidade = valorRestante / cedula.rawValue
-//
-//      resultado += "\(quantidade) nota(s) de \(cedula.rawValue)\n"
-//
-//      valorRestante = valorRestante % cedula.rawValue
-//    }
-//
-//  }
-//
-//  return resultado
-//}
-//
-//let valor = 8
-//let cedulasDisponiveis: [Cedula] = [.cinquenta, .dez, .cinco, .dois]
-//
-//print(saque(valor: valor, cedulasDisponiveis: cedulasDisponiveis))
 
 
