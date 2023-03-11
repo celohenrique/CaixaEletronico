7d04af22b656d9b52e6d2addce6222a615f50370
Marcelo Henrique <celohenriquech7@gmail.com>
2023-03-10 23:41:08 -0300

    fixed
    adicionado objeto para tratar informacoes que sao casadas

--- CaixaEletronico.playground/Contents.swift
+++ CaixaEletronico.playground/Contents.swift
@@ -1,10 +1,11 @@
-import UIKit
+struct Nota {
+    let valor: Int
+    var quantidade: Int
+}
 
-let cedulasDisponiveis = [50, 10, 5, 2]
+let notasDisponiveis = [Nota(valor: 50, quantidade: 3), Nota(valor: 10, quantidade: 3), Nota(valor: 5, quantidade: 3), Nota(valor: 2, quantidade: 3)]
 
-var stock = [3, 3, 3, 3]
-
-func saque(valor: Int, estoque: [Int]) -> String {
+func saque(valor: Int, estoque: [Nota]) -> String {
     var valor = valor
     var estoque = estoque
     if valor < 0 {
@@ -17,34 +18,37 @@ func saque(valor: Int, estoque: [Int]) -> String {
     
     var resultado: [String] = []
     
-    for i in 0..<cedulasDisponiveis.count {
-        let cedula = cedulasDisponiveis[i]
+    for i in 0..<notasDisponiveis.count {
+        let nota = notasDisponiveis[i]
         var count = 0
         
-        while valor >= cedula && estoque[i] > 0 {
-            valor -= cedula
-            estoque[i] -= 1
+        while valor >= nota.valor && estoque[i].quantidade > 0 {
+            valor -= nota.valor
+            estoque[i].quantidade -= 1
             count += 1
         }
         
         if count > 0 {
-            resultado.append("\(cedula)x\(count)")
+            resultado.append("\(nota.valor)x\(count)")
         }
     }
     
     if valor == 0 {
         return resultado.joined(separator: " | ")
     } else {
-        // adiciona as cédulas retiradas de volta no estoque
-        for i in 0..<cedulasDisponiveis.count {
-            estoque[i] += (resultado.filter{ $0.contains("\(cedulasDisponiveis[i])") }.count)
+        // adiciona as notas retiradas de volta no estoque
+        for i in 0..<notasDisponiveis.count {
+            let nota = notasDisponiveis[i]
+            estoque[i].quantidade += (resultado.filter{ $0.contains("\(nota.valor)") }.count)
         }
         return "Não é possível realizar o saque."
     }
 }
 
-print(saque(valor: 205, estoque: stock))
-print(saque(valor: 75, estoque: stock))
-print(saque(valor: 30, estoque: stock))
-print(saque(valor: 600, estoque: stock))
-print(saque(valor: 8, estoque: stock))
+print(saque(valor: 20, estoque: notasDisponiveis))
+print(saque(valor: 205, estoque: notasDisponiveis))
+print(saque(valor: 75, estoque: notasDisponiveis))
+print(saque(valor: 30, estoque: notasDisponiveis))
+print(saque(valor: 600, estoque: notasDisponiveis))
+print(saque(valor: 8, estoque: notasDisponiveis))
+print(saque(valor: 5, estoque: notasDisponiveis))
